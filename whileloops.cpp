2ec96194804f0fd2a097cd8ac2d1cf175d9c9a8e
stOneskull <stOne@skull.casa>
2025-09-08 20:02:33 +1000

    while loops

---
+++ whileloops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int pos = 0;
+    int endPos = 5;
+
+    while (pos < endPos) {
+        pos++;
+        std::cout << "Position: " << pos << std::endl;
+    }    
+}
+    
